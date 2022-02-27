3e4feaf353ba8a3e56ac840b6e11d39b3a9d98ae
hassan-raza <hassan.numl2013@gmail.com>
2022-02-27 07:54:39 +0500

    settin update v2

--- core/settings.py
+++ core/settings.py
@@ -121,6 +121,8 @@ STATICFILES_DIRS = (
     os.path.join(CORE_DIR, 'apps/static'),
 )
 
-
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 #############################################################
 #############################################################
